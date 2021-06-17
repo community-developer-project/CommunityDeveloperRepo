788a3b4338c4421e115ab7f00b037d1220de7bc7
vikas <vikaskurli88@gmail.com>
2021-06-17 17:14:00 +0530

    updated

--- DeveloperCommunityApp/src/main/java/com/devcom/boot/controller/FeedController.java
+++ DeveloperCommunityApp/src/main/java/com/devcom/boot/controller/FeedController.java
@@ -1,4 +1,5 @@
 package com.devcom.boot.controller;
+
 import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,67 +20,56 @@ import com.devcom.boot.service.FeedServiceInterface;
 @RestController
 @RequestMapping("developerCommunity/feed")
 public class FeedController {
-	
+
 	@Autowired
 	FeedServiceInterface service;
-	
+
 	@PostMapping("save")
-	public ResponseEntity<String> saveFeed(@RequestBody Feed feed){
-		 service.saveFeed(feed);
-	return new ResponseEntity<String>("Feed Added Successfully",HttpStatus.OK);
+	public ResponseEntity<String> saveFeed(@RequestBody Feed feed) {
+		service.saveFeed(feed);
+		return new ResponseEntity<String>("Feed Added Successfully", HttpStatus.OK);
 	}
-	
+
 	@GetMapping("{feedId}")
-	public ResponseEntity<Feed> getFeed(@PathVariable("feedId") int feedId){
+	public ResponseEntity<Feed> getFeed(@PathVariable("feedId") int feedId) {
 		Optional<Feed> feed = service.getFeedById(feedId);
 
-	return new ResponseEntity<Feed>(feed.get(),HttpStatus.OK);
+		return new ResponseEntity<Feed>(feed.get(), HttpStatus.OK);
 	}
-	
-	
-	
+
 	@GetMapping("/developer/{devId}")
-	public ResponseEntity<?> getFeedByDeveloper(@PathVariable("devId") int devId){
+	public ResponseEntity<?> getFeedByDeveloper(@PathVariable("devId") int devId) {
 		List<Feed> listOfFeeds = (List<Feed>) service.getFeedByDeveloperId(devId);
-		
-		return new ResponseEntity<Object>(listOfFeeds,HttpStatus.OK);
+
+		return new ResponseEntity<Object>(listOfFeeds, HttpStatus.OK);
 	}
-	
-	
+
 	@GetMapping("/topic/{topic}")
-	public ResponseEntity<?> getFeedByTopic(@PathVariable("topic") String topic){
+	public ResponseEntity<?> getFeedByTopic(@PathVariable("topic") String topic) {
 		List<Feed> listOfTopics = (List<Feed>) service.getFeedByTopic(topic);
-		
-		return new ResponseEntity<Object>(listOfTopics,HttpStatus.OK);
+
+		return new ResponseEntity<Object>(listOfTopics, HttpStatus.OK);
 	}
-	
-	
-	
-	
-	
+
 	@GetMapping("/keyword/{keyword}")
-	public ResponseEntity<?> getFeedByKeyword(@PathVariable("keyword") String keyword){
+	public ResponseEntity<?> getFeedByKeyword(@PathVariable("keyword") String keyword) {
 		List<Feed> listOfKeyword = (List<Feed>) service.getFeedByKeyword(keyword);
-		
-		return new ResponseEntity<Object>(listOfKeyword,HttpStatus.OK);
+
+		return new ResponseEntity<Object>(listOfKeyword, HttpStatus.OK);
 	}
-	
-	
-	
-	
+
 	@DeleteMapping("{feedId}")
-	public ResponseEntity<String> deleteFeed(@PathVariable("feedId") int feedId){
-		 service.deleteFeedById(feedId);
-		 return new ResponseEntity<String>("Feed Deleted Succesfully",HttpStatus.OK);
-		
+	public ResponseEntity<String> deleteFeed(@PathVariable("feedId") int feedId) {
+		service.deleteFeedById(feedId);
+		return new ResponseEntity<String>("Feed Deleted Succesfully", HttpStatus.OK);
+
 	}
-	
-	
+
 	@PutMapping("update")
-	public ResponseEntity<String> updateFeed(@RequestBody Feed feed){
-		 	service.updateFeed(feed);
-		 return new ResponseEntity<String>("Feed Updated Succesfully",HttpStatus.OK);
-		
+	public ResponseEntity<String> updateFeed(@RequestBody Feed feed) {
+		service.updateFeed(feed);
+		return new ResponseEntity<String>("Feed Updated Succesfully", HttpStatus.OK);
+
 	}
-	
+
 }
