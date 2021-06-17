529e7179242a2e89871471ef639c5ef788ea73c2
AgentMr47 <85739626+AgentMr47@users.noreply.github.com>
2021-06-17 17:41:47 +0530

    ResponseModule

--- DeveloperCommunityApp/src/main/java/com/devcom/boot/controller/AdminController.java
+++ DeveloperCommunityApp/src/main/java/com/devcom/boot/controller/AdminController.java
@@ -6,17 +6,13 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devcom.boot.entity.Admin;
 import com.devcom.boot.entity.Developer;
-import com.devcom.boot.entity.Feed;
 import com.devcom.boot.service.AdminServiceInterface;
 
 @RestController
@@ -56,23 +52,12 @@ public class AdminController {
 		return new ResponseEntity<Object>(developer,HttpStatus.OK);
 		
 	}
-
-	@DeleteMapping("{feedId}/remove")
-	public ResponseEntity<Object> deleteFeed(@PathVariable("feedId") Integer feedId){
-		 service.deleteFeedAdmin(feedId);
-	return new ResponseEntity<Object>("Feed Removed Successfully",HttpStatus.OK);
-	}
-	@GetMapping("getDev/{devId}/block")
-	public ResponseEntity<Object> blockDeveloper(@PathVariable("devId") Integer devId){ 
-		Developer developer = service.blockDeveloperById(devId);
+	
+	@GetMapping("getDev/{devId}/Invalidate")
+	public ResponseEntity<Object> invalidateDeveloper(@PathVariable("devId") Integer devId){ 
+		Developer developer = service.invalidateDeveloperById(devId);
 		return new ResponseEntity<Object>(developer,HttpStatus.OK);
 		
 	}
 	
-	@GetMapping("getDev/{devId}/unBlock")
-	public ResponseEntity<Object> unblockDeveloper(@PathVariable("devId") Integer devId){ 
-		Developer developer = service.unblockDeveloperById(devId);
-		return new ResponseEntity<Object>(developer,HttpStatus.OK);
-		
-	}
 }
---
+++ DeveloperCommunityApp/src/main/java/com/devcom/boot/exception/ResponseAlreadyExistsException.java
@@ -0,0 +1,9 @@
+package com.devcom.boot.exception;
+
+public class ResponseAlreadyExistsException extends RuntimeException {
+	
+	public ResponseAlreadyExistsException(String message) {
+		super(message);
+	}
+
+}
